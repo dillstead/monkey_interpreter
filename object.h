a540a137a723196cb1f91fe9b2bf94e97af54d6f
Kris Kujawski <kkujawski@blackberry.com>
2020-10-26 14:12:32 -0400

    Add evaluation of return statements

--- object.h
+++ object.h
@@ -7,6 +7,7 @@ enum object_type
 {
     INTEGER_OBJ,
     BOOLEAN_OBJ,
+    RETURN_VALUE,
     NULL_OBJ
 };
 
@@ -29,6 +30,12 @@ struct boolean_object
     char *inspect;
 };
 
+struct return_value
+{
+    enum object_type type;
+    struct object *value;
+};
+
 struct null_object
 {
     enum object_type type;
@@ -43,5 +50,6 @@ void object_destroy(struct object *object);
 char *object_inspect(struct object *object);
 struct integer_object *integer_object_alloc(long long value);
 struct boolean_object *boolean_object_alloc(bool value);
+struct return_value *return_value_alloc(struct object *value);
 struct null_object *null_object_alloc(void);
 #endif
